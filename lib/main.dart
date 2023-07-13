07181e325802f2f66c4ab640334860434229d6c5
Ajay poly <ajaypoly99@gmail.com>
2023-07-13 10:20:28 +0530

    added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,33 @@ import 'package:flutter/material.dart';
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(205, 8, 194, 163),
 );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 0, 28, 28),
+);
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData().copyWith(
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+              backgroundColor: kDarkColorScheme.primaryContainer)
+              ,
+        ),
+      ),
+      theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -27,11 +50,15 @@ void main() {
               backgroundColor: kColorScheme.primaryContainer),
         ),
         textTheme: ThemeData().textTheme.copyWith(
-                titleLarge: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: kColorScheme.onSecondaryContainer,
-              fontSize: 16,
-            ))),
-    home: const Expenses(),
-  ));
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+      // themeMode: ThemeMode.system ,
+      home: const Expenses(),
+    ),
+  );
 }
